89264b50dfbd7aa60e8bc63c1d7232002246d2da
HaSa1002 <johawitt@outlook.de>
2018-12-21 18:58:41 +0100

    added load constructor added default constructor added dummy load method added node selection added node moving added node deselection

--- Storytool/Project.cpp
+++ Storytool/Project.cpp
@@ -1,6 +1,11 @@
 #include "Project.hpp"
 
 namespace st {
+	Project::Project(const std::string & path) { 
+		load(path);
+	}
+
+
 	xmlData st::Project::save() {
 		xmlData result { "project",
 			{{"name", name }}, "", {
@@ -8,6 +13,34 @@ namespace st {
 		}
 		};
 		return std::move(result);
+	}
+
+
+	void Project::load(const std::string & path) { 
+		
+	}
+
+	bool Project::hasActiveNode() {
+		return active != nullptr;
+	}
+
+	void Project::moveNode(const sf::Vector2i & dest) { 
+		if (hasActiveNode())
+			active->setPosition(static_cast<sf::Vector2f>(dest));
+	}
+
+	void Project::selectNode(const sf::Vector2i & pos) { 
+	active = nullptr;
+		for (auto& graph : graphs) {
+			if (Node* n = graph.second.hitNode(pos)) {
+				active = n;
+				return;
+			}
+		}
+	}
+
+	void Project::deselectNode() {
+		active = nullptr;
 	}
 
 }
--- Storytool/Project.hpp
+++ Storytool/Project.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include "Config.hpp"
 #include "Character.hpp"
-#include "ActionSet.hpp"
 #include "NodeSet.hpp"
 #include "Graph.hpp"
 #include "Parser.hpp"
 
 namespace st {
 	struct Project {
+	Project() :name{"Untitled Project"} {};
+	Project(const std::string& path);
 		std::string name;
-		ActionSet actionSet;
 		std::unordered_map<id, NodeSet> node_sets;
 		std::unordered_map<id, Graph> graphs;
 		std::unordered_map<st::name, Character> characters;
@@ -17,9 +17,17 @@ namespace st {
 		std::unordered_map<id, std::string> storyline;
 		std::string storyline_id_template;
 
-
+		//Save and load
 		xmlData save();
-		
+		void load(const std::string& path);
+
+
+		//Input Handler Functions
+		Node* active = nullptr;
+		bool hasActiveNode();
+		void moveNode(const sf::Vector2i& dest);
+		void selectNode(const sf::Vector2i& pos);
+		void deselectNode();
 
 
 	};
