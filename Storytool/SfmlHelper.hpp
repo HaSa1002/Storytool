5ce7dceec4aebda11b467cfc863f869a922a5455
HaSa1002 <johawitt@outlook.de>
2018-12-22 01:20:26 +0100

    added length calculation

--- Storytool/SfmlHelper.hpp
+++ Storytool/SfmlHelper.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics/Rect.hpp>
+#include <cmath>
 
 namespace sf {
 	template<class T>
@@ -10,5 +11,13 @@ namespace sf {
 		r.left -= offset;
 		r.height += offset;
 		r.width += offset;
+	}
+
+	template<class T>
+	static T length(const Vector2<T>& v);
+
+	template<class T>
+	static T length(const Vector2<T>& v) {
+		return static_cast<T>(std::hypot<T, T>(v.x, v.y));
 	}
 }
