d542e737af263065d41607bbd7b4906944b50ce2
HaSa1002 <johawitt@outlook.de>
2018-12-30 03:58:04 +0100

    added Version consts

--- Storytool/Config.hpp
+++ Storytool/Config.hpp
@@ -10,4 +10,23 @@ namespace st {
 	using id = std::string;
 	using xmlData = af::XML::Structure;
 	using GlobalVars = std::unordered_map<name, double>;
+	
+	//Program Version
+	constexpr auto VERSION = "1.0-DEV";
+	/*
+		M = Major
+		m = Minor
+		T = Type
+			0  = Development (-DEV)
+			1  = Pre-Release (-PRE)
+			2  = Public ()
+			2+ = Hotfixes (-2+) (i.e: -3, -5, ...)
+		Format: MMmmT
+	*/
+	constexpr auto VERSION_NUM = 01000;
+	
+	//File Format
+	constexpr auto FILE_VERSION = 1;
+	constexpr auto MIN_FILE_VER = 1;
+
 }
